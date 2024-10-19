b0e5a839eb5a8f7b512b086d9d124e1f25951448
Yann Ntigura <ganza.ntigura@student.uclouvain.be>
2024-10-19 23:41:51 +0200

    README

--- run.sh
+++ run.sh
@@ -1,12 +1,26 @@
 #!/bin/zsh
 
+# Commands to create your github profile 
+# Copy-paste this code in your local codespace and save it as "run.sh" in your desktop 
+# Now go in your shell and give persmission to execute this code with this prompt:
+# chmod 700 run.sh
+# Execute the code with this prompt:
+# ./run.sh
+
 echo "We are going to create a github profile!"
 echo ""
 
-mkdir profile
+cd /Path/to/your/Desktop
 
-cd /Your/path/profile 
-git pull
-git add --all
-git commit -m "Shell script"
-git push
+mkdir your_username
+
+cd /Your/path/your_username
+
+# You must already have a created repository and change your_username with your actual username
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main 
+git remote add origin https://github.com/your_username/your_username.git
+git push -u origin main
